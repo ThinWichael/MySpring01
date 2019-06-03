133fa20cc6648b1b09aa0d81f38fef225fbd4748
MichaelChin <airkolf0327@gmail.com>
2019-06-03 20:48:41 +0800

    DB config

--- src/main/java/hello/service/DAO/jdbc/SqlServer.java
+++ src/main/java/hello/service/DAO/jdbc/SqlServer.java
@@ -13,7 +13,7 @@ public class SqlServer {
 
 	static {	
 		DriverManagerDataSource dataSource=new DriverManagerDataSource();
-        dataSource.setUrl("jdbc:sqlserver://localhost:1433;databaseName=TutorialDB");
+        dataSource.setUrl("jdbc:sqlserver://123.192.97.36:1433;databaseName=master");//databaseName=TutorialDB
         dataSource.setDriverClassName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
         dataSource.setUsername("sa");//test1
         dataSource.setPassword("123weR456");//1234567
